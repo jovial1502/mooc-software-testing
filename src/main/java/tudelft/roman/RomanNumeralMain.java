54713a40f4e76a7cb7c9b53b640cb23f76c05b0d
jovial1502 <jovial1502@gmail.com>
2021-02-11 17:25:57 +0100

    Complete the num finder task

---
+++ src/main/java/tudelft/roman/RomanNumeralMain.java
@@ -0,0 +1,12 @@
+package tudelft.roman;
+
+
+public class RomanNumeralMain {
+
+    public static void main (String[] args) {
+        RomanNumeral rn = new RomanNumeral();
+
+        System.out.println(rn.convert("vxx"));
+
+    }
+}
