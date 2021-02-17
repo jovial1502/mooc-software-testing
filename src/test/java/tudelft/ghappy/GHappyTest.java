d050e1d77ca8e369dfca5d25fb8416efd0d9522e
jovial1502 <jovial1502@gmail.com>
2021-02-17 17:58:41 +0100

    Complete the GHappyTest

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,49 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy happy;
+
+    @BeforeEach
+    public void gHappyInit() {
+        this.happy = new GHappy();
+    }
+
+    @Test
+    public void oneTimeHappy() {
+        happy.gHappy("xxggxx");
+    }
+
+    @Test
+    public void oneTimeUnhappy() {
+        happy.gHappy("xxgxx");
+    }
+
+    @Test
+    public void manyTimesHappy() {
+        happy.gHappy("xxggxxggxx");
+    }
+
+    @Test
+    public void manyTimesHappyOneTimeNot() {
+        happy.gHappy("xxggxxgxx");
+    }
+
+    @Test
+    public void oneG() {
+        happy.gHappy("g");
+    }
+
+    @Test
+    public void noGs() {
+        happy.gHappy("xxxx");
+    }
+
+    @Test
+    public void nullString() {
+        happy.gHappy(null);
+    }
 }
